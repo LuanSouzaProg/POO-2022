dc737a223ebb94414f03cc7b02119e0e6c520fc9
LuanSouzaProg <luandesouza2001@rede.ulbra.br>
2022-03-11 21:02:45 -0300

    FirstCommit

---
+++ Aula02TestAmbiente/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Aula02TestAmbiente
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
